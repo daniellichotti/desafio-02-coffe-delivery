b68dddf39420e0dcb1505de0f850e32f7e9e2163
daniellichotti <danielclichotti@gmail.com>
2024-02-10 17:03:08 -0300

    Implementar o Texto do Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,62 @@
-import { MainContainer, MainContainerMessage } from './styled'
+import {
+  CoffeeIcon,
+  MainContainer,
+  MainContainerMessage,
+  PackageIcon,
+  ShopCartIcon,
+  TextContainerMessage,
+  TextDetailsMessage,
+  TimerIcon,
+  TitleSubtitleMessage,
+} from './styled'
+import cupCoffeeMain from '../../assets/cupCoffeeMain.svg'
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
 
 export function Main() {
   return (
     <MainContainer>
-      <MainContainerMessage />
+      <MainContainerMessage>
+        <TextContainerMessage>
+          <TitleSubtitleMessage>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </TitleSubtitleMessage>
+          <TextDetailsMessage>
+            <div>
+              <div>
+                <ShopCartIcon>
+                  <ShoppingCart weight="fill" />
+                </ShopCartIcon>
+                <p>Compra simples e segura</p>
+              </div>
+              <div>
+                <PackageIcon>
+                  <Package weight="fill" />
+                </PackageIcon>
+                <p>Embalagem mantém o café intacto</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <TimerIcon>
+                  <Timer weight="fill" />
+                </TimerIcon>
+                <p>Entrega rápida e rastreada</p>
+                <CoffeeIcon>
+                  <Coffee weight="fill" />
+                </CoffeeIcon>
+                <p>O café chega fresquinho até você</p>
+              </div>
+            </div>
+          </TextDetailsMessage>
+        </TextContainerMessage>
+        <div>
+          <img src={cupCoffeeMain} alt="" />
+        </div>
+      </MainContainerMessage>
     </MainContainer>
   )
 }
--- src/components/Main/styled.ts
+++ src/components/Main/styled.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import mainBackground from '../../assets/Background.png'
 
 export const MainContainer = styled.main``
 
@@ -7,8 +6,103 @@ export const MainContainerMessage = styled.main`
   width: 100vw;
   height: 544px;
 
-  background-image: url(${mainBackground});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const TextContainerMessage = styled.div`
+  width: 588px;
+  height: 500px;
+
+  color: black;
+  display: flex;
+  flex-direction: column;
+`
+
+export const TitleSubtitleMessage = styled.div`
+  h1 {
+    margin-top: 92px;
+    color: ${(props) => props.theme['base-title']};
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 48px;
+    line-height: 130%;
+  }
+  p {
+    width: 588px;
+    height: 52px;
+    margin-top: 16px;
+
+    color: ${(props) => props.theme['base-subtitle']};
+    font-stretch: 100;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 130%;
+  }
+`
+
+export const TextDetailsMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+
+  margin-top: 90px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+  }
+`
+
+export const ShopCartIcon = styled.div`
+  width: 32px;
+  height: 32px;
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme['yellow-dark']};
+  border-radius: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const PackageIcon = styled.div`
+  width: 32px;
+  height: 32px;
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme['base-text']};
+  border-radius: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const TimerIcon = styled.div`
+  width: 32px;
+  height: 32px;
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme.yellow};
+  border-radius: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const CoffeeIcon = styled.div`
+  width: 32px;
+  height: 32px;
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme.purple};
+  border-radius: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
